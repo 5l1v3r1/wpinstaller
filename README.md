b9104bf6c9b03a97e6e3294781c12e277b077b9f
Permalink Group <support@permalinkgroup.com>
2014-07-18 02:01:11 -0500

    Initial commit, basic funciontality created

---
+++ README.md
@@ -0,0 +1,9 @@
+Wordpress Files Installer
+=========================
+
+Simple installer for wordpress file, avoid the annoying process of files and upload and uncompress etc.
+
+Requirements
+------------
+
+Any Linux hosting with linux console. 
---
+++ wpinstaller.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_GET['dir'])){
+	shell_exec('mkdir ' . $_GET['dir']);
+	$targetdir = getcwd() . '/' . $_GET['dir'];
+}else{
+	$targetdir = getcwd();
+}
+
+shell_exec ('wget http://wordpress.org/latest.zip');
+shell_exec ('unzip latest.zip');
+shell_exec ('mv '. getcwd() . '/wordpress/* ' . $targetdir);
+shell_exec ('find ' . $targetdir . ' -type d -exec chmod 755 {} \;' );
+shell_exec ('find ' . $targetdir . ' -type f -exec chmod 644 {} \;' );
+shell_exec ('rm -r ' . getcwd() . '/wordpress/');
+shell_exec ('rm ' . getcwd() . '/latest.zip');
+shell_exec ('rm ' . getcwd() . '/wpinstaller.php');
